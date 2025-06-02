d01b857e03d571ed81d63301ea444362f33c4b42
danieldumar <daniel.dumarques@gamil.com>
2025-06-02 16:40:00 -0300

    Adicionei a pagina editar usuário e corrigi o bug da foto de perfil

---
+++ Views/Usuarios/EditarPerfilUsuario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LanceCertoSQL.Views.Usuarios
+{
+    public class EditarPerfilUsuarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
