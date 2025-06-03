995e1c1862904b3d5929f6d4dc72cd3451f65e8c
allanbarbosa01 <crash171@gmail.com>
2025-06-02 23:20:55 -0300

    feat: criado CadastroController com métodos GET e POST para exibir e processar formulário de cadastro

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,26 @@
+﻿using LanceCertoSQL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanceCertoSQL.Controllers
+{
+    public class CadastroController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CadastroUsuarioViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+            return RedirectToAction("Index");
+        }
+    }
+}
