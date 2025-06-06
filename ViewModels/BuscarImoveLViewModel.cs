436ffb25efcf44ca2be211045cc7bed160e000e3
danieldumar <daniel.dumarques@gamil.com>
2025-06-06 17:18:32 -0300

    feat(busca): implementada página de busca de imóveis e pagina de registro de imoveis

---
+++ ViewModels/BuscarImoveLViewModel.cs
@@ -0,0 +1,24 @@
+﻿using LanceCertoSQL.Models;
+
+namespace LanceCertoSQL.ViewModels
+{
+    public class BuscarImovelViewModel
+    {
+        public string? Cidade { get; set; }
+        public string? UsuarioId { get; set; }
+        public string? FaixaValor { get; set; }
+        public string? TemLeilaoAtivo { get; set; } // "Sim", "Não" ou null
+
+        public List<Imovel> Resultados { get; set; } = new List<Imovel>();
+
+        public List<string> FaixasValor { get; set; } = new List<string>
+        {
+            "<400000",
+            "400000-900000",
+            ">900000"
+        };
+
+        public List<Usuario> UsuariosDisponiveis { get; set; } = new List<Usuario>();
+    }
+}
+
